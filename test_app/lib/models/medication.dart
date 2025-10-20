f6defd3dd58e9e482da04f37321cd065e53524f4
amartinn <amartinn@umich.edu>
2025-10-19 21:11:58 -0400

    medication page front-end (Progress for everyone to view on 10/20)

--- test_app/lib/models/medication.dart
+++ test_app/lib/models/medication.dart
@@ -2,34 +2,31 @@ class Medication {
   final String id;
   final String name;
   final String dosage;
-  final DateTime nextDose;
-  final bool isTaken;
-  final String frequency;
+  final List<String> timeslotIds;
+  final Map<String, bool> isTakenByTimeslot;
 
   Medication({
     required this.id,
     required this.name,
     required this.dosage,
-    required this.nextDose,
-    this.isTaken = false,
-    this.frequency = 'Daily',
-  });
+    required this.timeslotIds,
+    Map<String, bool>? isTakenByTimeslot,
+  }) : this.isTakenByTimeslot = isTakenByTimeslot ??
+            {for (var tsId in timeslotIds) tsId: false};
 
   Medication copyWith({
     String? id,
     String? name,
     String? dosage,
-    DateTime? nextDose,
-    bool? isTaken,
-    String? frequency,
+    List<String>? timeslotIds,
+    Map<String, bool>? isTakenByTimeslot,
   }) {
     return Medication(
       id: id ?? this.id,
       name: name ?? this.name,
       dosage: dosage ?? this.dosage,
-      nextDose: nextDose ?? this.nextDose,
-      isTaken: isTaken ?? this.isTaken,
-      frequency: frequency ?? this.frequency,
+      timeslotIds: timeslotIds ?? this.timeslotIds,
+      isTakenByTimeslot: isTakenByTimeslot ?? this.isTakenByTimeslot,
     );
   }
 }
---
+++ test_app/lib/models/timeslot.dart
@@ -0,0 +1,6 @@
+class Timeslot {
+  final String label;
+  final String id;
+
+  Timeslot({required this.label, required this.id});
+}
